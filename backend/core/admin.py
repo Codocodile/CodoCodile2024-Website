4e41ee22646d205697db84f8af39b657dc931057
ahjafar <amir138106@gmail.com>
2024-10-31 16:53:44 +0330

    export group data

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.contrib import admin
 
 from core import models
 
 import secrets
+import csv
 
 def populate_judge_fields(modeladmin, request, queryset):
     for group in queryset:
@@ -10,9 +12,21 @@ def populate_judge_fields(modeladmin, request, queryset):
         group.judge_password = secrets.token_urlsafe(12)
         group.save()
 
+def export_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=group_data.csv'
+    
+    writer = csv.writer(response)
+    writer.writerow(['name', 'Judge Username', 'Judge Password'])
+    
+    for group in queryset:
+        writer.writerow([group.id, group.judge_username, group.judge_password])
+    
+    return response
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'judge_username', 'judge_password']
-    actions = [populate_judge_fields]
+    actions = [populate_judge_fields, export_to_csv]
 
 class VisitModelAdmin(admin.ModelAdmin):
     readonly_fields = ['date_created'] 
@@ -24,4 +38,6 @@ admin.site.register(models.Challenger)
 admin.site.register(models.Group, GroupAdmin)
 admin.site.register(models.Membership)
 admin.site.register(models.Visit, VisitModelAdmin)
+
+
 
