a698b459b1fa0d704d0f56803c92d87c690fac85
Moeein Aali <moeeeinaali@gmail.com>
2024-11-22 21:43:33 +0330

    fix url

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -376,8 +376,11 @@ const Profile = () => {
                     variant="gradient"
                     className={`flex h-10  items-center w-full justify-center gap-3 mt-3 mb-4 bg-blue-900`}
                     onClick={() => {
-                        window.open(`https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=2nd+Codocodile+Contest&organizationId=98884196&issueYear=2024&issueMonth=11&certUrl=https://api.codocodile.ir/api/get-cert?user_id%3d${data.id}`, `_blank`);
+                        const certUrl = encodeURIComponent(`https://api.codocodile.ir/api/get-cert?user_id=${data.id}`);
+                        const url = `https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=2nd+Codocodile+Contest&organizationId=98884196&issueYear=2024&issueMonth=11&certUrl=${certUrl}`;
+                        window.open(url, `_blank`);
                     }}
+
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-linkedin" viewBox="0 0 16 16">
