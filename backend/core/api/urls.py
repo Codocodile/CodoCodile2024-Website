c738e6ccd8753390a9614e3e019b3f1fde6dcd8c
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2025-11-18 21:06:23 +0330

    fix shit

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -7,18 +7,44 @@ from rest_framework_simplejwt.views import (
 from core.api import views
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('create-challenger/', views.ChallengerCreateAPIView.as_view(), name='add-challenger'),
-    path('cv/', views.ChallengerCVAPIView.as_view(), name='add-cv'),
-    path('confirm-challenger/', views.ChallengerConfirmAPIView.as_view(), name='confirm-challenger'),
-    path('update-challenger/', views.ChallengerUpdateAPIView.as_view(), name='update-challenger'),
-    path('view-challenger/', views.ChallengerViewAPIView.as_view(), name='view-challenger'),
-    path('search-challenger/', views.ChallengerSearchAPIView.as_view(), name='search-challenger'),
-    path('team/', views.GroupAPIView.as_view(), name='team'),
-    path('invitation/', views.InvitationRequestAPIView.as_view(), name='invitation'),
-    path('accept-invitation/', views.InvitationAcceptanceAPIView.as_view(), name='accept-invitation'),
-    path('password-reset/', views.PasswordResetAPIView.as_view(), name='password-reset'),
-    path('visit/', views.VisitCreateAPIView.as_view(), name='visit'),
-    path('get-cert/', views.CertAPIView.as_view(), name='get-cert'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "create-challenger/",
+        views.ChallengerCreateAPIView.as_view(),
+        name="add-challenger",
+    ),
+    path("cv/", views.ChallengerCVAPIView.as_view(), name="add-cv"),
+    path(
+        "confirm-challenger/",
+        views.ChallengerConfirmAPIView.as_view(),
+        name="confirm-challenger",
+    ),
+    path(
+        "update-challenger/",
+        views.ChallengerUpdateAPIView.as_view(),
+        name="update-challenger",
+    ),
+    path(
+        "view-challenger/",
+        views.ChallengerViewAPIView.as_view(),
+        name="view-challenger",
+    ),
+    path(
+        "search-challenger/",
+        views.ChallengerSearchAPIView.as_view(),
+        name="search-challenger",
+    ),
+    path("team/", views.GroupAPIView.as_view(), name="team"),
+    path("invitation/", views.InvitationRequestAPIView.as_view(), name="invitation"),
+    path(
+        "accept-invitation/",
+        views.InvitationAcceptanceAPIView.as_view(),
+        name="accept-invitation",
+    ),
+    path(
+        "password-reset/", views.PasswordResetAPIView.as_view(), name="password-reset"
+    ),
+    path("visit/", views.VisitCreateAPIView.as_view(), name="visit"),
+    path("get-cert/", views.CertAPIView.as_view(), name="get-cert"),
 ]
